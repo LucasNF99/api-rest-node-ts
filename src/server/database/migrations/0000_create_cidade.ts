436a37f4f44ae8f1604a41b68beadcaf9338acab
Lucas Natalicio Ferreira <lucasnf99@gmail.com>
2023-04-06 11:32:40 -0300

    feat: create city migration

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,21 @@
+
+import { Knex } from 'knex';
+import { ETableNames } from '../ETablesNames';
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable(ETableNames.CITY, table => {
+    table.bigIncrements('id').primary().index();
+    table.string('nome', 150).index().notNullable();
+
+    table.comment('Table used for storage cities to our sitem.');
+  }).then(() => {
+    console.log(`# Create table ${ETableNames.CITY}`);
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.CITY).then(() => {
+    console.log(`# Delete table ${ETableNames.CITY}`);
+  });
+}
+
